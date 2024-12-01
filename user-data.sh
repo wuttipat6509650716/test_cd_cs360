1d9245ab59e9674dd434171bcb827a5a78fcc4d6
wuttipat6509650716 <144822298+wuttipat6509650716@users.noreply.github.com>
2024-12-02 01:25:15 +0700

    Update user-data.sh

--- user-data.sh
+++ user-data.sh
@@ -4,8 +4,3 @@ sudo yum update -y
 sudo yum install -y docker
 sudo service docker start
 sudo usermod -aG docker ec2-user
-
-sudo yum install -y amazon-ssm-agent
-sudo systemctl start amazon-ssm-agent
-sudo systemctl enable amazon-ssm-agent
-sudo systemctl restart amazon-ssm-agent
